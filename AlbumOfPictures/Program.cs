c6cb5a230f269e4037fa27a2afcb95601273e130
operator_IO <vova.chigorin@yandex.ru>
2024-11-03 16:47:15 +0300

    Добавьте файлы проекта.

---
+++ AlbumOfPictures/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlbumOfPictures
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int albumImages = 52;
+            int maxImagesInRow = 3;
+
+            float rowsFilled = Convert.ToSingle(albumImages) / maxImagesInRow;
+            float imagesRemains = rowsFilled - Convert.ToInt32(rowsFilled);
+
+            Console.WriteLine($"Количество заполненных рядов: {Convert.ToInt32(rowsFilled)}\n" +
+                              $"Сверх мера картинок: {imagesRemains}");
+        }
+    }
+}
