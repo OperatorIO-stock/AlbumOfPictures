f9ad413be8c8327c443f28ad17602ac6baf386c7
operatorIO-stock <vova.chigorin@yandex.com>
2024-11-09 14:40:48 +0300

    first commit

---
+++ AlbumOfPictures/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlbumOfPictures
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int images = 52;
+            int maxImagesInRow = 3;
+
+            int rowsFilled = images / maxImagesInRow;
+            float imagesRemains = images & maxImagesInRow;
+
+            Console.WriteLine($"Количество заполненных рядов: {rowsFilled}\n" +
+                              $"Сверх мера картинок: {imagesRemains}");
+        }
+    }
+}
